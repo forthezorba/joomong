721857af99de4ff758686341d18039ad524111ab
zorbaKT <forthezorba@gmail.com>
2020-08-04 21:38:19 +0900

    Updates

---
+++ precache-manifest.45d17641056a27b8dc2153e6826c5d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b2ff8b416a760cb143ba2439ab74a7a",
+    "url": "/forthezorba/joomong.git/index.html"
+  },
+  {
+    "revision": "61f9e87a7085418723ab",
+    "url": "/forthezorba/joomong.git/static/css/2.ea4a5e64.chunk.css"
+  },
+  {
+    "revision": "55c7ffc7c37c34da4a6d",
+    "url": "/forthezorba/joomong.git/static/css/main.5cf99681.chunk.css"
+  },
+  {
+    "revision": "61f9e87a7085418723ab",
+    "url": "/forthezorba/joomong.git/static/js/2.46e5fc88.chunk.js"
+  },
+  {
+    "revision": "ddf74bf492199c05a8932df93ec2be9f",
+    "url": "/forthezorba/joomong.git/static/js/2.46e5fc88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55c7ffc7c37c34da4a6d",
+    "url": "/forthezorba/joomong.git/static/js/main.947fb544.chunk.js"
+  },
+  {
+    "revision": "9c3521cf9e01df83e656",
+    "url": "/forthezorba/joomong.git/static/js/runtime-main.2f754b30.js"
+  }
+]);
