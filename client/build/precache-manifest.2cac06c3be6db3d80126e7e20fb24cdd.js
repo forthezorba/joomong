db8a143a232fe93be7d45cb843451565616f4629
zorbaKT <forthezorba@gmail.com>
2020-08-05 19:36:07 +0900

    'build'

---
+++ client/build/precache-manifest.2cac06c3be6db3d80126e7e20fb24cdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c036325edbe0eb9091901a16dabc216",
+    "url": "/ec2-3-34-3-253.ap-northeast-2.compute.amazonaws.com/index.html"
+  },
+  {
+    "revision": "23b321f6e37c9ce53fe8",
+    "url": "/ec2-3-34-3-253.ap-northeast-2.compute.amazonaws.com/static/css/2.ea4a5e64.chunk.css"
+  },
+  {
+    "revision": "8fa1c5e3be64f7e98299",
+    "url": "/ec2-3-34-3-253.ap-northeast-2.compute.amazonaws.com/static/css/main.5cf99681.chunk.css"
+  },
+  {
+    "revision": "23b321f6e37c9ce53fe8",
+    "url": "/ec2-3-34-3-253.ap-northeast-2.compute.amazonaws.com/static/js/2.5bf15811.chunk.js"
+  },
+  {
+    "revision": "ddf74bf492199c05a8932df93ec2be9f",
+    "url": "/ec2-3-34-3-253.ap-northeast-2.compute.amazonaws.com/static/js/2.5bf15811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa1c5e3be64f7e98299",
+    "url": "/ec2-3-34-3-253.ap-northeast-2.compute.amazonaws.com/static/js/main.dce14349.chunk.js"
+  },
+  {
+    "revision": "005e4f7562ee13dc15ee",
+    "url": "/ec2-3-34-3-253.ap-northeast-2.compute.amazonaws.com/static/js/runtime-main.0dcd80f3.js"
+  }
+]);
